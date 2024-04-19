08dec4b3e066f03eac0fd3fc09fc8d1b86a13f86
Kangho <kkh4323@naver.com>
2024-04-19 21:16:15 +0900

    password, profileImg

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -8,6 +9,32 @@ export class AuthService {
 
   // 회원가입
   async signinUser(createUserDto: CreateUserDto) {
-    return await this.userService.createUser(createUserDto);
+    try {
+      // const saltValue = await bcrypt.genSalt(10);
+      // const hashedPassword = await bcrypt.hash(
+      //   createUserDto.password,
+      //   saltValue,
+      // );
+      // const createdUser = await this.userService.createUser({
+      //   ...createUserDto,
+      //   password: hashedPassword,
+      // });
+      // createdUser.password = undefined;
+      // return createdUser;
+      const createdUser = await this.userService.createUser(createUserDto);
+      createdUser.password = undefined;
+      return createdUser;
+    } catch (error) {
+      if (error?.code === 23305) {
+        throw new HttpException(
+          'user with that email already exists',
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+      throw new HttpException(
+        'something went wrong',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
